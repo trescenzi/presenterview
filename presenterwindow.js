508a2fb8cd1b84d0af5c280fe53694dae6900457
Thomas <trescenzi@gmail.com>
2016-02-22 20:05:20 -0500

    feat(presenterWindow): add timer

--- presenterwindow.js
+++ presenterwindow.js
@@ -5,11 +5,13 @@ window.addEventListener('storage', function() {
     update();
 });
 
+window.setInterval(updateTime, 1000);
+
 window.addEventListener('keydown', function(e) {
   if (e.keyCode === 39) {
-    localStorage.setItem('arrowRight', localStorage.getItem('arrowRight') + 1 || 1)
+    localStorage.setItem('arrowRight', localStorage.getItem('arrowRight') + 1 || 1);
   } else if (e.keyCode === 37) {
-    localStorage.setItem('arrowLeft', localStorage.getItem('arrowLeft') + 1 || 1)
+    localStorage.setItem('arrowLeft', localStorage.getItem('arrowLeft') + 1 || 1);
   }
 });
 
@@ -30,6 +32,20 @@ for (var i = 0; i <= stylesheets.length - 1; i++) {
 function update() {
     var note = document.getElementById('note');
     note.innerHTML = localStorage.getItem('notes');
+}
+/**
+ * Increment timer by 1 second
+ */
+var timeElapsed = 0;
+function updateTime() {
+  timeElapsed = timeElapsed + 1;
+  var hours = Math.floor(timeElapsed / 3600);
+  var minutes = Math.floor((timeElapsed - (hours * 3600)) / 60);
+  var seconds = timeElapsed - (hours * 3600) - (minutes * 60);
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  document.getElementById('timer').textContent = hours + ':' + minutes + ':' + seconds;
 }
 /**
  * initially run the update
